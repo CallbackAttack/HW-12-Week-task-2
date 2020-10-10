fed8cd94475463cec11357463bc19c0567fb0380
Yan <yan.doshchinskiy@gmail.com>
2020-10-10 13:11:48 +0700

    title dash

--- client/components/home.js
+++ client/components/home.js
@@ -9,7 +9,7 @@ const Home = () => {
       <Header />
       <div className="flex items-center justify-center h-screen">
         <div id="title" className="bg-indigo-800 text-white font-bold rounded-lg border shadow-lg p-10">
-          Dashboard
+          <title>Dashboard</title>
           <Link to="/dashboard/profile/3c3add9b-1f1f-4760-a66d-df0e9fb8fcdb"> Go To Profile </Link>
           <Link to="/dashboard/main"> Go To Main </Link>
         </div>
@@ -22,4 +22,4 @@ const Home = () => {
 Home.propTypes = {}
 
 
-export default React.memo(Home) 
+export default React.memo(Home)
