2fec0a3a4a5010382f8b758cb17f51818f465567
Yan <yan.doshchinskiy@gmail.com>
2020-10-11 22:00:29 +0700

    all titles chanhed

--- client/components/dash-main.js
+++ client/components/dash-main.js
@@ -7,8 +7,8 @@ const DashMain = () => {
   return (
     <div>
       <div className="flex items-center justify-center h-screen">
-        <div id="title" className="bg-indigo-800 text-white font-bold rounded-lg border shadow-lg p-10">
-          Main
+        <div className="bg-indigo-800 text-white font-bold rounded-lg border shadow-lg p-10">
+          <div id="title">Main</div>
           <Link to="/dashboard/profile/3c3add9b-1f1f-4760-a66d-df0e9fb8fcdb"> Go To Profile </Link>
           <Link to="/dashboard/"> Go To Root </Link>
         </div>
--- client/components/dash-profile.js
+++ client/components/dash-profile.js
@@ -10,8 +10,8 @@ const DashProfile = () => {
   return (
     <div>
       <div className="flex items-center justify-center h-screen">
-        <div id="title" className="bg-indigo-800 text-white font-bold rounded-lg border shadow-lg p-10">
-          Profile
+        <div className="bg-indigo-800 text-white font-bold rounded-lg border shadow-lg p-10">
+          <div id="title">Profile</div>
           <Link to="/dashboard/"> Go To Root </Link>
           <Link to="/dashboard/main"> Go To Main </Link>
           <div id="username">{profileName}</div>
